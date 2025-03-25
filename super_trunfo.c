237e74298e312a3bea0ba8b5196951b59447cf99
Davi Rocha <102175365+Pongo013@users.noreply.github.com>
2025-03-25 17:01:37 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main() {
+    char estado, codCarta[3], nomeCid[30];
+    int populacao, ptTuristicos;
+    float area, pib;
+    char estado2, codCarta2[3], nomeCid2[30];
+    int populacao2, ptTuristicos2;
+    float area2, pib2;
+
+    printf("=== Olá! Bem-vindo ao jogo SuperTrunfo! ===\n");
+    printf("= Para começar, insira as seguintes informações da primeira carta! =\n");
+    printf("Qual o estado da carta? (Digite apenas 1 letra)\n");
+    scanf(" %s", estado);
+    printf("Qual o código da carta? (Insira a letra acima seguida de 2 números)\n");
+    scanf(" %s", codCarta);
+    printf("Qual o nome da cidade na carta?\n");
+    scanf(" %s", nomeCid);
+    printf("Qual a população total da cidade?\n");
+    scanf(" %i", &populacao);
+    printf("Qual a área (em km²) total do território dessa cidade?\n");
+    scanf(" %f", &area);
+    printf("Qual o PIB da cidade?\n");
+    scanf(" %f", &pib);
+    printf("Qual o número de pontos turísticos que tem na cidade?\n");
+    scanf(" %i", &ptTuristicos);
+    printf("\n");
+
+    printf("=== Agora insira as informações para a segunda carta! ===\n");
+    printf("Qual o estado da 2º carta? (Digite apenas 1 letra)\n");
+    scanf(" %s", estado2);
+    printf("Qual o código da 2º carta? (Insira a letra acima seguida de 2 números)\n");
+    scanf(" %s", codCarta2);
+    printf("Qual o nome da cidade na 2º carta?\n");
+    scanf(" %s", nomeCid2);
+    printf("Qual a população total da 2º cidade?\n");
+    scanf(" %i", populacao2);
+    printf("Qual a área (em km²) total do território dessa 2º cidade?\n");
+    scanf(" %f", area2);
+    printf("Qual o PIB da 2º cidade?\n");
+    scanf(" %f", pib2);
+    printf("Qual o número de pontos turísticos que tem na 2º cidade?\n");
+    scanf(" %i", ptTuristicos2);
+    printf("\n");
+
+    printf("=== A seguir, as informações inseridas! ===\n");
+    printf("== Carta1 ==\n");
+    printf("Estado: %s\n", estado);
+    printf("Código da carta: %s\n", codCarta);
+    printf("Nome da Cidade: %s\n", nomeCid);
+    printf("População: %i\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Número de Pontos Turísticos: %i\n", ptTuristicos);
+    printf("\n");
+    printf("=== Agora da segunda carta! ===\n");
+    printf("== Carta2 ==\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código da carta: %s\n", codCarta2);
+    printf("Nome da Cidade: %s\n", nomeCid2);
+    printf("População: %i\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %i\n", ptTuristicos2);
+
+}
